18cef2eb57395d75887681f9354487207a97fa67
Hoyoung <hoyoung@MacBook-Pro.local>
2019-03-10 18:03:40 -0700

    first message

---
+++ index.js
@@ -0,0 +1,40 @@
+//var a = 10%8*3/2
+//alert(a)
+
+alert("Welcome To Summoner's Rift")
+
+
+//var arrayA = [1, 2, 5, 6, 10]
+//alert(arrayA);
+//goes with
+//arrayA.push("Herro");
+//alert(arrayA)
+
+
+//var boo = false && true || true && true
+//alert(boo)
+
+
+/*function addSquares(x, y) {
+	return(x*x + y*y)
+}
+*/
+//alert(addSquares(1009, 2678));
+
+
+/*function concat(str1,str2) {
+	return(str1 + str2);
+}
+
+//alert(concat("Hello ", "World"))
+
+
+var newstring = concat("Hello", "World");
+
+alert(newstring);
+
+function triProduct(x, y, z) {
+	alert(x*y*z)
+}
+alert(triProduct(10,11,12))
+*/
